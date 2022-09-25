f876019a34e1e801f1867ac731b5a9401ed402ee
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-25 08:10:10 -0300

    fix installed apps

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "evaluator",
+    "interaction",
     "rest_framework",
     "django_filters",
     "corsheaders",
