46e70b6e28e2062907798fb82a67cd70c6ea4fa8
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-25 08:11:41 -0300

    implements util functions

--- interaction/utils.py
+++ interaction/utils.py
@@ -1,10 +1,10 @@
 import random
-from .models import Connection
+from .models import InteractionConnection
 
 def create_new_pin_number():
                 not_unique = True
                 while not_unique:
                     unique_pin = random.randint(10000, 99999)
-                    if not Conection.objects.filter(Referrence_Number=unique_pin):
+                    if not InteractionConnection.objects.filter(Referrence_Number=unique_pin):
                         not_unique = False
                 return str(unique_pin)
