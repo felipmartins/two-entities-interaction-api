1ee34a98d7daf67c299c6954f843547736a4c655
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-26 13:49:53 -0300

    create routes for particpant and connection creation

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import check_if_participants_exists, create_participant
+from .views import check_if_participants_exists, create_participant, check_if_connection_exists
 
 urlpatterns = [
     path('', check_if_participants_exists ,name='check-participant'),
-    path('create_participant', create_participant, name='create-participant')
+    path('create_participant', create_participant, name='create-participant'),
+    path('check_connection', check_if_connection_exists, name='check-connection'),
 ]
