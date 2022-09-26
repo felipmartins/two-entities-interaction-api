59ab245f46d2ee0e21333cf64cb5f9ef314c5041
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-26 11:54:46 -0300

    add route to urls for create participant function

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import check_if_participants_exists
+from .views import check_if_participants_exists, create_participant
 
 urlpatterns = [
-    path('', check_if_participants_exists ,name='check'),
+    path('', check_if_participants_exists ,name='check-participant'),
+    path('create_participant', create_participant, name='create-participant')
 ]
