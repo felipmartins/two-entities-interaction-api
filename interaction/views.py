d6cc0d2d7df67d39e82b7c013fec6442613244d6
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-26 09:25:13 -0300

    add signature functions to views

--- interaction/views.py
+++ interaction/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def check_if_participants_exists(
+    request,
+):  # verifica se existe e caso exista retorna mensagem de sucesso e pedido de número
+    if (
+        request.method == "GET"
+    ):  # para conexão, caso contrário, pede para a pessoa dizer seu nome para criar a pessoa participante;
+        ...
+
+
+def create_participant(
+    request,
+):  # recebe via post o alexaid e o nome da pessoa e cria o participante
+    if request.method == "POST":
+        ...
+
+
+def check_if_connection_exists(request):
+    ...
+
+
+def check_number_of_participants(request):
+    ...
+
+
+def check_whos_turn_is(request):
+    ...
+
+
+def send_message(request):
+    ...
