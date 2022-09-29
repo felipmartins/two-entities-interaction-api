e857741bc8b85b896df181a397a752608beb77ec
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-29 12:04:38 -0300

    add messenger to admin

--- interaction/admin.py
+++ interaction/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Participant, InteractionConnection
+from .models import Messenger, Participant, InteractionConnection
 
 
 admin.site.register(Participant)
 admin.site.register(InteractionConnection)
+admin.site.register(Messenger)
