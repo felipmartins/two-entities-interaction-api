3a44b1133ea9fd550871a15ce1742fadab347418
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-29 12:05:22 -0300

    add routes

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import check_if_participants_exists, create_participant, check_if_connection_exists
+from .views import check_if_participants_exists, create_participant, check_if_connection_exists, create_connection, send_message
 
 urlpatterns = [
     path('', check_if_participants_exists ,name='check-participant'),
     path('create_participant', create_participant, name='create-participant'),
     path('check_connection', check_if_connection_exists, name='check-connection'),
+    path('create_connection', create_connection, name="create-connection" ),
+    path('send_message', send_message, name="send-message" )
 ]
